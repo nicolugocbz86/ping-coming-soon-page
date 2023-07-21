b9169f5421f098c7a7b0f3bcc3ccb2917fb7e252
nicolugocbz86 <cbzramok@gmail.com>
2023-07-21 02:15:21 -0300

    new styles

--- js/index.js
+++ js/index.js
@@ -1,11 +1,15 @@
 const inputs = document.getElementsByTagName('input');
-let pAlert = document.getElementById('pAlert');
+let divAlert = document.getElementById('div-alert');
+let pAlert = document.getElementById('p-alert');
 
 inputs[1].addEventListener('click', () => {
 
     if (inputs[0].innerHTML == false) {
 
         pAlert.style.display = 'block';
+        pAlert.style.color = 'hsl(354, 100%, 66%)';
+        pAlert.style.fontStyle = 'italic';
+        pAlert.style.fontSize = '.8rem';
         inputs[0].style.borderColor = 'hsl(354, 100%, 66%)';
 
     }
