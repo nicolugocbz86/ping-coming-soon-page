43d8cd55a2145c48fe6c94d10b9ec40b1f458862
nicolugocbz86 <cbzramok@gmail.com>
2023-07-21 05:40:55 -0300

    final styles

--- js/index.js
+++ js/index.js
@@ -7,9 +7,6 @@ inputs[1].addEventListener('click', () => {
     if (inputs[0].innerHTML == false) {
 
         pAlert.style.display = 'block';
-        pAlert.style.color = 'hsl(354, 100%, 66%)';
-        pAlert.style.fontStyle = 'italic';
-        pAlert.style.fontSize = '.8rem';
         inputs[0].style.borderColor = 'hsl(354, 100%, 66%)';
 
     }
